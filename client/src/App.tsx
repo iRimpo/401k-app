e17b6afd14cf79a4a748c8bf05038049c5474dea
Richard Azucenas <richardazucenas@berkeley.edu>
2025-11-21 18:23:42 -0800

    setup

---
+++ client/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+
+function App() {
+  return (
+    <div>
+      <h1>401(k) Contribution Manager</h1>
+      
+    </div>
+  );
+}
+
+export default App
