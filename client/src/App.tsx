ce4010eb6b7aea3b0c597e9fe0c1d5d5943ea281
Richard Azucenas <richardazucenas@berkeley.edu>
2025-11-21 19:21:28 -0800

    working button component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-
+import { Button } from "@/components/ui/button"
 
 function App() {
   return (
     <div>
       <h1>401(k) Contribution Manager</h1>
-      
+      <Button onClick={() => alert('Button clicked!')}>
+        Save Contribution Rate
+      </Button>
     </div>
   );
 }
